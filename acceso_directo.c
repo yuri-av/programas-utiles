2e670e822d0b83f0b04bc51132f6afdafcda19f6
yuri-av <arancibiavieirayuri@gmail.com>
2025-11-04 20:42:41 -0300

    Update acceso_directo.c

--- acceso_directo.c
+++ acceso_directo.c
@@ -27,6 +27,7 @@ void cargar_archivo(char archivo[], char modo[]){
     else{
         /*preguntar por fuera el indice, de manera que si es == -1
         no entre, si es distinto pregunta el resto de los datos y vuelve a preguntar el indice*/
+        printf("----------------------------------\n\n");
         printf("Ingrese el indice: ");
         scanf("%i", &persona.i);
         scanf("%*[^\n]%*c");
@@ -39,10 +40,9 @@ void cargar_archivo(char archivo[], char modo[]){
             printf("Ingrese el nombre: ");
             fgets(persona.nombre, MAX_NOMBRE, stdin);
             scanf("%*[^\n]%*c");
-            printf("%s", persona.nombre);
+
             printf("Ingrese el DNI: ");
             fgets(persona.dni, MAX_DNI, stdin);
-            printf("%s", persona.dni);
             scanf("%*[^\n]%*c");
 
             printf("Ingrese el sexo: ");
@@ -61,6 +61,7 @@ void cargar_archivo(char archivo[], char modo[]){
             printf("Ingrese el indice: ");
             scanf("%i", &persona.i);
         }
+    printf("----------------------------------\n\n");
     fclose(pf);
     }
 }
@@ -76,28 +77,93 @@ void leer_archivo_binario(char archivo[], char modo[]){
         printf("Donde quiere buscar: ");
         scanf("%i", &buscador);
         scanf("%*[^\n]%*c");
-
+        printf("----------------------------------\n\n");
         fseek(pf, sizeof(rpersona)*buscador, SEEK_SET);
         fread(&persona, sizeof(rpersona), 1, pf);
-        printf("I: %i\tNombre: %s\t DNI: %s\tSexo: %i\tEdad: %i\n",persona.i, persona.nombre, persona.dni,persona.sexo, persona.edad);
+        printf("I: %i\tNombre: %s DNI: %s\tSexo: %i\tEdad: %i\n",persona.i, persona.nombre, persona.dni,persona.sexo, persona.edad);
     }
     fclose(pf);
 }
 
+void modificacion(int posicion, char archivo[]){
+    rpersona persona;
+    int sn = 0;
+    persona.i = posicion;
+    FILE*pf;
+    pf= fopen(archivo, "wb");
+
+    if (pf == NULL)
+    {
+        printf("Se produjo un error al abrir el archivo\n");
+    }
+    else{
+        printf("Confirmar la modificacion 1:S/2:N: ");
+        scanf("%i", &sn);
+        printf("----------------------------------\n\n");
+        if (sn == 1)
+        {
+            printf("Ingrese el nombre: ");
+            fgets(persona.nombre, MAX_NOMBRE, stdin);
+            scanf("%*[^\n]%*c");
+
+            printf("Ingrese el DNI: ");
+            fgets(persona.dni, MAX_DNI, stdin);
+            scanf("%*[^\n]%*c");
+
+            printf("Ingrese el sexo: ");
+            scanf("%i", &persona.sexo);
+            scanf("%*[^\n]%*c");
+
+            printf("Ingrese la edad: ");
+            scanf("%i", &persona.edad);
+            scanf("%*[^\n]%*c");
+            
+            //Pido donde escribir el registro en el archivo
+            //calcula los bytes
+            fseek(pf, sizeof(rpersona)*persona.i, SEEK_SET); 
+            //escribe el archivo 
+            fwrite(&persona, sizeof(rpersona), 1,pf); 
+            printf("----------------------------------\n\n");
+        }
+        fclose(pf);
+    }
+
+}
+
 int main(){
-    int leer = 0;
+    int decision = 0;
+    int posicion = 0;
     char archivo[MAX_NOMBRE] = {"personas.dat"};
     char modo[10];
+    printf("----------------------------------\n\n");
+
     printf("\tModo\nwb, rb: ");
     fgets(modo, 2, stdin);
 
     cargar_archivo(archivo, modo);
     printf("Quiere leer? 1 SI 2 NO: ");
-    scanf("%i", &leer);
-    if (leer == 1)
+    scanf("%i", &decision);
+    if (decision == 1)
     {
         strcpy(modo, "rb");
         leer_archivo_binario(archivo, modo);
     }
+    /*Para el archivo creado en el punto anterior, 
+    crear una función que reciba una posición y 
+    datos de una persona, y reemplace en la fila 
+    indicada en la posición los nuevos datos. */
+    decision = 0;
+    printf("Desea modificar un registro? 1:S/0:N: ");
+    scanf("%i", &decision);
+    scanf("%*[^\n]%*c");
+    if (decision == 1)
+    {
+        printf("En que posicion?: ");
+        scanf("%i", &posicion);
+        scanf("%*[^\n]%*c");
+        modificacion(posicion, archivo);
+        leer_archivo_binario(archivo, "rb");
+    }
+    
     return 0;
 }
